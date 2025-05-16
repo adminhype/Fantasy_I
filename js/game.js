55c37391a7d1aff6ad880b40f83e83d8d7691649
Adem <ado.designs@outlook.de>
2025-05-17 01:06:37 +0200

    Implemented first class

--- js/game.js
+++ js/game.js
@@ -1,17 +1,11 @@
 let canvas;
 let ctx;
-let character = new Image();
+let character = new MovableObject();
 
 function init() {
     canvas = document.getElementById('canvas');
-    // getContext 2d object, canvas = ctx
     ctx = canvas.getContext('2d');
-    // Copy Relative Path img\main-heroes\PNG\Knight\knight.png
-    // .. übergeordneten ordner zugreifen
-    character.src = '../img/main-heroes/PNG/Knight/knight.png';
 
-    setTimeout(function () {
-        ctx.drawImage(character, 20, 20, 50, 150); // koordinaten + breite + höhe
-    }, 2000);
+    console.log('My Character is ', character);
 
 } 
---
+++ js/models/movableObject.class.js
@@ -0,0 +1,12 @@
+// Vorlage 
+class MovableObject {
+    x = 120;
+    y = 400;
+    img;
+
+
+    moveRight() {
+        console.log('Moving-right');
+
+    }
+}
