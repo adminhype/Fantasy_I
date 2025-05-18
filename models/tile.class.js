d7c195ddfdea5ebb908327ba9125dcd71213ef02
Adem <ado.designs@outlook.de>
2025-05-18 16:12:44 +0200

    Added tile to the map

---
+++ models/tile.class.js
@@ -0,0 +1,13 @@
+class Tile extends MovableObject {
+    height = 50;
+    width = 50;
+    y = 250;
+    x = 200;
+
+
+
+    constructor() {
+        super().loadImage('img/main-world/PNG/tiles/tile32.png');
+
+    }
+}
--- models/word.class.js
+++ models/word.class.js
@@ -6,6 +6,11 @@ class World {
         new Skeleton(),
         new Skeleton(),
     ];
+    tiles = [
+        new Tile(),
+        new Tile(),
+        new Tile()
+    ];
     canvas;
     ctx;
 
@@ -19,9 +24,14 @@ class World {
     draw() {
         self = this;
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
         this.ctx.drawImage(this.char.img, this.char.x, this.char.y, this.char.height, this.char.width) // alle variablen z.b aus world mit this öffnen 
         this.enemies.forEach((enemy) => {
             this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
+        });
+
+        this.tiles.forEach((tile) => {
+            this.ctx.drawImage(tile.img, tile.x, tile.y, tile.width, tile.height);
         });
 
         requestAnimationFrame(() => {
